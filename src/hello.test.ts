00efb88d46cb8f4644d29b30431e4d18bcd22873
Hans Yulian <hansyulian@windowslive.com>
2025-07-31 08:51:24 +0700

    chore: broke some format

--- src/hello.test.ts
+++ src/hello.test.ts
@@ -1,7 +1,7 @@
 import { sayHello } from './hello';
 
 test('sayHello returns correct greeting', () => {
-  expect(sayHello('World')).toBe('Hello, World!');
+        expect(sayHello('World')).toBe('Hello, World!');
 });
 test('sayHello returns correct greeting 2', () => {
   expect(sayHello('Hans')).toBe('Hello, Hans!');
